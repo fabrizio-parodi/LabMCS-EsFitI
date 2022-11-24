b5a09e01c69b5c725a5adf49836d316d340d4014
fabrizio parodi <fabrizio.parodi@ge.infn.it>
2022-11-24 11:28:12 +0100

    update

--- fitlin.py
+++ fitlin.py
@@ -1,6 +1,6 @@
 from   ROOT    import *
-from   numpy   import *
-from   ctypes  import *
+import numpy   as np
+import ctypes  as ct
 
 def func(x,a,b):
     return a*x+b
@@ -11,21 +11,21 @@ def fcn(npar, gin, f, par,iflag):
         chi2 += ((y[i]-func(x[i],par[0],par[1]))/ey[i])**2
     f.value = chi2
 
-x  = array([]); y  = array([]); ex = array([]); ey = array([])
+x  = np.array([]); y  = np.array([]); ex = np.array([]); ey = np.array([])
 for line in open("pendolo.dat"):
     dt  = line.split()
     if len(dt)!=4:
         continue
-    x   = append(x,  float(dt[0])); y   = append(y,  float(dt[1]))
-    ex  = append(ex, float(dt[2])); ey  = append(ey, float(dt[3]))
+    x   = np.append(x,  float(dt[0])); y   = np.append(y,  float(dt[1]))
+    ex  = np.append(ex, float(dt[2])); ey  = np.append(ey, float(dt[3]))
 
 minuit = TMinuit(2)
 minuit.SetFCN(fcn);
 minuit.DefineParameter(0,'par0',4,0.01,0.,0.)
 minuit.DefineParameter(1,'par1',0,0.01,0.,0.)
 minuit.Command("MIGRAD")
-a  = c_double(0.0); b  = c_double(0.0)
-ea = c_double(0.0); eb = c_double(0.0)
+a  = ct.c_double(0.0); b  = ct.c_double(0.0)
+ea = ct.c_double(0.0); eb = ct.c_double(0.0)
 minuit.GetParameter(0,a,ea);
 minuit.GetParameter(1,b,eb);
 
--- fitlin_iminuit.py
+++ fitlin_iminuit.py
@@ -1,5 +1,5 @@
 from   iminuit import Minuit
-from   numpy   import *
+import numpy as np
 
 def f(x,a,b):
     return a*x+b
@@ -10,11 +10,11 @@ def chi2(par):
         val = val + ((y[i]-f(x[i],par[0],par[1]))/ey[i])**2
     return val
 
-x  = array([]); y  = array([]); ex = array([]); ey = array([])
+x  = np.array([]); y  = np.array([]); ex = np.array([]); ey = np.array([])
 for line in open("pendolo.dat"):
     dt  = line.split() 
     if len(dt)!=4:
         continue
-    x   = append(x,  float(dt[0])); y   = append(y,  float(dt[1]))
-    ex  = append(ex, float(dt[2])); ey  = append(ey, float(dt[3]))
+    x   = np.append(x,  float(dt[0])); y   = np.append(y,  float(dt[1]))
+    ex  = np.append(ex, float(dt[2])); ey  = np.append(ey, float(dt[3]))
 
